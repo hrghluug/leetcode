62eee6194bea36f1b4e45b98f61cefa3d37e426c
xgh <xieguanghua@fjhb.com>
2022-08-29 20:49:47 +0800

    q80

--- src/main/java/com/xgh/doublepointer/Q80.java
+++ src/main/java/com/xgh/doublepointer/Q80.java
@@ -1,10 +1,27 @@
 package com.xgh.doublepointer;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+
 /**
  * @author xgh 2022/8/29
  */
 public class Q80 {
+    @Test
+    public void test(){
+        int[] ints = {1, 1, 1, 2, 2, 3};
+        removeDuplicates(ints);
+        System.out.println(Arrays.toString(ints));
+    }
     public int removeDuplicates(int[] nums) {
-
+        int p=0;
+        for (int i = 0; i < nums.length; i++) {
+           if (p>1&&nums[i]==nums[p-1]&&nums[i]==nums[p-2]){
+               continue;
+           }
+           nums[p++]=nums[i];
+        }
+        return p-1;
     }
 }
